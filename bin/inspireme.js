70bc037f075e9a3b4b2c7430d67df40a9127edc3
Justin Lonas <sdwrage@gmail.com>
2017-10-20 22:06:10 -0400

    Fixing merge

--- bin/inspireme.js
+++ bin/inspireme.js
@@ -1,41 +1,45 @@
 #! /usr/bin/env node
 
-var rndSong = require('rnd-song');
-var yargs = require('yargs').argv;
-var fs = require('fs');
+const fs = require('fs');
+const getHomePath = require('home-path');
+const rndSong = require('rnd-song');
+const yargs = require('yargs').argv;
 
-var nameLength = Math.floor((Math.random() * 12) + 6);
-var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-var path = home + "/.inspireme-key";
+let nameLength = Math.floor((Math.random() * 12) + 6);
+const path = getHomePath() + '/.inspireme-key';
 
-if(yargs.wordcount > 0) {
-    nameLength = parseInt(yargs.wordcount);
+if (yargs.wordcount > 0) {
+  nameLength = parseInt(yargs.wordcount);
 }
 
-var apikey = '';
+let apikey = '';
 if (fs.existsSync(path)) {
-    apikey = fs.readFileSync(path, 'utf8');
+  apikey = fs.readFileSync(path, 'utf8');
 } else {
-    console.log("You need to set your API key with the command: inspireme-addkey ADDYOURKEYHERE");
-    process.exit(1);
+  console.log('You need to set your API key with the command: inspireme-addkey --key ADDYOURKEYHERE');
+  process.exit(1);
 }
 
-var reversed = yargs.reversed;
-var options = {
-    api_key: apikey,
-    snippet: true,
-    language: 'en'
-  };
-   
-rndSong(options, function(err, res) {
-    if (!err) {
-      var snippet = res.snippet.snippet_body;
-      snippet = snippet.replace(/[^a-zA-Z]/g, "").toLowerCase();
-      if (reversed) {
-          snippet = snippet.split("").reverse().join("");
-      }
-      var start = Math.floor(Math.random() * ((snippet.length - 1) - nameLength));
-      var end = ((start + 1) + nameLength > snippet.length) ? snippet.length - start : start + nameLength;
-      console.log(snippet.substring(start, end));
-    }
-  });
+let reversed = yargs.reversed;
+const options = {
+  api_key: apikey,
+  snippet: true,
+  language: 'en'
+};
+
+rndSong(options, (err, res) => {
+  if (!err) {
+    let snippet = res.snippet.snippet_body;
+    snippet = snippet.replace(/[^a-zA-Z]/g, '').toLowerCase();
+
+    if (reversed) snippet = snippet.split('').reverse().join('');
+
+    const start = Math.floor(Math.random() * ((snippet.length - 1) - nameLength));
+
+    const end = ((start + 1) + nameLength > snippet.length)
+      ? snippet.length - start
+      : start + nameLength;
+
+    console.log(snippet.substring(start, end));
+  }
+});
